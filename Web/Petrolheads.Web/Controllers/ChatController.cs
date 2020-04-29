079f980e43aa334fd8c5f10df3914b2b80d22c00
Mario Vasilev <41074548+MarioVasilev99@users.noreply.github.com>
2020-04-30 00:12:40 +0300

    Fixed minor styling bugs.

--- Web/Petrolheads.Web/Controllers/ChatController.cs
+++ Web/Petrolheads.Web/Controllers/ChatController.cs
@@ -1,9 +1,11 @@
 ﻿namespace Petrolheads.Web.Controllers
 {
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
     public class ChatController : Controller
     {
+        [Authorize]
         public IActionResult Index()
         {
             return this.View();
